bdfa7a73a8b1bddd10c2a5175f3c8ae2e6bdf479
Trofymenko Vladyslav <naughtysora@proton.me>
2026-01-07 00:29:23 +0200

    tests for LRU peek,has,delete

--- test/LRU.js
+++ test/LRU.js
@@ -31,4 +31,48 @@ describe('LRU', () => {
     assert.equal(cache.get('b'), undefined);
   });
 
+  it('evict', () => {
+    const cache = new LRU(3);
+    cache.set('a', 1);
+    cache.set('b', 2);
+    cache.set('c', 3);
+    cache.evict();
+    assert.equal(cache.size, 2);
+    assert.equal(cache.get('a'), undefined);
+  });
+
+  it('has', () => {
+    const cache = new LRU(3);
+    cache.set('a', 1);
+    cache.set('b', 2);
+    cache.set('c', 3);
+    assert.equal(cache.has('a'), true);
+    cache.evict();
+    assert.equal(cache.size, 2);
+    assert.equal(cache.has('b'), false);
+    assert.equal(cache.get('b'), undefined);
+  });
+
+  it('peek', () => {
+    const cache = new LRU(3);
+    cache.set('a', 1);
+    cache.set('b', 2);
+    cache.set('c', 3);
+    assert.equal(cache.peek('a'), true);
+    cache.evict();
+    assert.equal(cache.peek('a'), false);
+    assert.equal(cache.size, 2);
+    assert.equal(cache.get('a'), undefined);
+  });
+
+  it('delete', () => {
+    const cache = new LRU(3);
+    cache.set('a', 1);
+    cache.set('b', 2);
+    cache.set('c', 3);
+    cache.delete('b');
+    cache.delete('g');
+    assert.equal(cache.size, 2);
+    assert.equal(cache.get('b'), undefined);
+  });
 });
